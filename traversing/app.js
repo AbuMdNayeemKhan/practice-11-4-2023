ede4cb500c84f20bee4114d9acc4c121a72375f1
Abu Md Nayeem Khan <abumdnayeemkhan@gmail.com>
2023-04-11 10:52:21 +0600

    learning

---
+++ traversing/app.js
@@ -0,0 +1,9 @@
+// const pNote = document.querySelector(".pNote");
+
+// console.log(pNote.parentNode);
+
+const parentDiv = document.querySelector(".parentDiv");
+
+// console.log(parentDiv.firstElementChild);
+// console.log(parentDiv.lastElementChild);
+console.log(parentDiv.children);
